db6341c0014a4542381dd0fee2c14eaeaba01b46
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-20 12:44:38 +0800

    Home page boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+
 function App() {
-	return <></>;
+	return (
+		<main className='bg-black'>
+			<Navbar />
+			<Hero />
+			<Highlights />
+		</main>
+	);
 }
 
 export default App;
