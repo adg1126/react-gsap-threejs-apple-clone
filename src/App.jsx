00af440621df8f177dc28d111118042751a20725
adg1126 <gutierrez.almiradyan@gmail.com>
2024-03-20 15:34:59 +0800

    Updated using new prettier style

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import Hero from './components/Hero';
 import Highlights from './components/Highlights';
 
 function App() {
-	return (
-		<main className='bg-black'>
-			<Navbar />
-			<Hero />
-			<Highlights />
-		</main>
-	);
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App;
